a2000da162d394cfa8302d82ad5b3f285c5d148c
ChesyaWhite <vyacheslavbelyai@gmail.com>
2020-03-22 08:54:31 +0300

    init: start task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str,bracketsConfig) {
+        let a = str.length;
+        if (a % 2 == 0) {
+                for (let i = 1; i <= (a/2); i++) {
+                        bracketsConfig.forEach(element => {
+                                if (str.includes(element.join(''))) {
+                                        str = str.replace(element.join(''), '')
+                                }
+                        });
+                }
+        }
+        if (str.length == 0 ) {
+                return true;
+        } else { 
+                return false;
+        }
 }
